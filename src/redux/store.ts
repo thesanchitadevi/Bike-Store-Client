c327f9eae427a1a7a3810fdd66eb446735230a6d
Sanchita Devi <thesanchitadevi@gmail.com>
2025-01-29 03:09:43 +0600

    redux initialize

---
+++ src/redux/store.ts
@@ -0,0 +1,42 @@
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { baseApi } from "./api/baseApi";
+import authReducer from "./features/auth/authSlice";
+
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    // auth: authReducer,
+    auth: persistedAuthReducer,
+  },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
+});
+
+// 'RootState' is the type of the whole state of the store
+export type RootState = ReturnType<typeof store.getState>;
+// 'AppDispatch' is the type of the dispatch function
+export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
